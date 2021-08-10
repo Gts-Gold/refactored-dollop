d0dec8bbfd9272db360f9795c01f9a6577dc91b8
warayut.ta <warayut.ta@princhealth.com>
2021-08-10 23:42:00 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,13 @@
 import TheLayout from '../components/TheLayout'
 import ThePopup from '../components/ThePopup';
 import HomePage from './home/HomePage'
-import Document, { Html, Head, Main, NextScript } from 'next/document'
-
 export default function Home() {
 	return (
-		<Html>
-			<Head>
-				<script
-					async
-					src={`https://www.googletagmanager.com/gtag/js?id=UA-204484351-1`}
-				/>
-				<script
-					dangerouslySetInnerHTML={{
-						__html: `window.dataLayer = window.dataLayer || [];
-							function gtag(){dataLayer.push(arguments);}
-							gtag('js', new Date());
-							gtag('config', 'UA-204484351-1');
-          				`,
-					}}
-				/>
-			</Head>
-			<div className="bg-image">
-				<TheLayout>
-					<HomePage />
-				</TheLayout>
-				{/* <ThePopup/> */}
-			</div>
-		</Html>
+		<div className="bg-image">
+			<TheLayout>
+				<HomePage/>
+			</TheLayout>
+			{/* <ThePopup/> */}
+		</div>
 	);
 }
